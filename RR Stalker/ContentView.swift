ba37f52e320f27fb09865a54248e24fbae239fac
gcctryg <gcctryg@gmail.com>
2026-05-18 14:24:17 -0700

    Initial Commit

---
+++ RR Stalker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RR Stalker
+//
+//  Created by Xuelu Feng on 5/18/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RR Stalker/RR_StalkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RR_StalkerApp.swift
+//  RR Stalker
+//
+//  Created by Xuelu Feng on 5/18/26.
+//
+
+import SwiftUI
+
+@main
+struct RR_StalkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
